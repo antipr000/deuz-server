1052c915df0d7f36fd12f0777611b692e02757f5
Soham <rockstarsam1234@gmail.com>
2024-06-15 21:10:55 +0530

    Uploaded deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
 
-# Replace with your server's SSH username and IP address or hostname
-SERVER_SSH_USER="root"
-SERVER_IP="159.203.188.95"
-DESTINATION_PATH="/root"
-CURRENT_DIR_NAME="$(basename "$PWD")"
-FINAL_PATH="/root/deuz-server"
+# Transfer the secrets
+scp firebase_credentials.py root@159.203.188.95:/root/deuz-server
 
-# Compress all files and folders in the current directory excluding this script
-tar --exclude="$0" -czvf "${CURRENT_DIR_NAME}".tar.gz .
-
-# Transfer the compressed file
-scp "${CURRENT_DIR_NAME}".tar.gz "$SERVER_SSH_USER"@"$SERVER_IP":"$DESTINATION_PATH"
-
-# Clean up the compressed file from local machine if needed
-rm "${CURRENT_DIR_NAME}".tar.gz
-
-ssh "$SERVER_SSH_USER"@"$SERVER_IP" << EOF
-tar -xzvf "${CURRENT_DIR_NAME}".tar.gz -C "${FINAL_PATH}"
-EOF
