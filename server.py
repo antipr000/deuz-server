175d22bc8746e3586fa4fffd857d4b04f5a45e9f
Soham <rockstarsam1234@gmail.com>
2024-06-16 15:51:08 +0530

    Added code for image upscale

--- server.py
+++ server.py
@@ -6,9 +6,12 @@ import io
 from firebase_admin.auth import verify_id_token
 from firebase_admin import initialize_app, credentials
 from firebase_credentials import cert_dict
+from super_image import EdsrModel, ImageLoader
 import requests
+import os
 
 app = Flask(__name__)
+app.config["MAX_CONTENT_LENGTH"] = 6 * 1024 * 1024  # 6 MB limit
 
 
 @app.route("/auth", methods=["POST"])
@@ -47,6 +50,36 @@ def remove_bg():
         img_byte_arr = io.BytesIO()
         output_image.save(img_byte_arr, format="PNG")
         img_byte_arr.seek(0)
+        return send_file(img_byte_arr, mimetype="image/png")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/upscale", methods=["POST"])
+@cross_origin()
+def upscale():
+    if "image" not in request.files:
+        return jsonify({"error": "No image found in request"}), 400
+
+    file = request.files["image"]
+
+    if file.filename == "":
+        return jsonify({"error": "No selected image"}), 400
+    file_name = file.filename
+    print("Here", file_name)
+    try:
+        scale_factor = request.form.get("scale", default=2, type=int)
+        input_image = Image.open(file.stream)
+        model = EdsrModel.from_pretrained("eugenesiow/edsr-base", scale=scale_factor)
+        inputs = ImageLoader.load_image(input_image)
+        preds = model(inputs)
+        file_name = file_name.split(".")[0] + ".png"
+        ImageLoader.save_image(preds, file_name)
+        img_byte_arr = io.BytesIO()
+        pil_image = Image.open(file_name)
+        pil_image.save(img_byte_arr, format="PNG")
+        img_byte_arr.seek(0)
+        os.remove(file_name)
         return send_file(img_byte_arr, mimetype="image/png")
     except Exception as e:
         return jsonify({"error": str(e)}), 500
